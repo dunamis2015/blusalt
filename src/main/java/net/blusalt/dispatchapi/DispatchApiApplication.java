ba2e302b84dbe56642c9f27012384e30bffbc90d
sheggz <segun@rank.africa>
2024-04-14 14:50:11 +0100

    Project-setup

---
+++ src/main/java/net/blusalt/dispatchapi/DispatchApiApplication.java
@@ -0,0 +1,13 @@
+package net.blusalt.dispatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/blusalt/dispatchapi/DispatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.blusalt.dispatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
